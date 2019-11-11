222891e0830ab57b2c1441c8a358bb07b005ae88
smie15352353 <xiaoshq6@mail2.sysu.edu.cn>
2019-11-11 15:32:01 -0800

    blog frontend

--- src/components/BlogList/BlogPage.js
+++ src/components/BlogList/BlogPage.js
@@ -4,6 +4,8 @@ import './index.css';
 import {Link} from "react-router-dom";
 
 class Blog extends Component {
+
+
     render() {
         var blogTitle = "党的十九大明确宣告：经过长期努力，中国特色社会主义进入了新时代。";
         var blogContent = "回溯千年，星云浩瀚。如今这金秋玉树银花，锦绣山河画卷，是烽烟四起时先辈抛洒热血，胼手胝足换来的，华夏儿女生与斯长于斯，光荣与梦想，皆与祖国紧紧相连，不可分割。世事变迁，青山不老，赤心不变。" +
