7eac5ba2c2713f6b08697062d5436cbf38bd87b7
SkylarWen <skylarvane6729@gmail.com>
2019-11-14 13:45:20 -0800

    change account

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PasswordChangePage from '../PasswordChange/index';
 import { AuthUserContext, withAuthorization, withEmailVerification } from '../Session';
 import 'antd/dist/antd.css';
-import BoughtCourse from './BoughtCourse';
+
 import { compose } from 'recompose';
 
 class AccountPage extends Component {
@@ -17,7 +17,6 @@ class AccountPage extends Component {
                     <div>
                         <h1>Account: {data.authUser.email}</h1>
                         <PasswordChangePage />
-                        <BoughtCourse />
                     </div>
                 )}
             </AuthUserContext.Consumer>
--- src/components/TakeCourse/BoughtCourse.js
+++ src/components/TakeCourse/BoughtCourse.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import './index.css';
+import '../Account/index.css';
 import { Tabs, List, Avatar, Icon} from 'antd';
 
 const { TabPane } = Tabs;
